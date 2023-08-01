572f8766974131f614f52ccb8060ea68ddecef3f
Uriel Gartzia <ugartziasola@gmail.com>
2023-08-01 20:55:22 +0200

    done

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ app.get('/', (req, res) => {
   res.send('Hello uri')
 })
 
-
-
 app.get("/home",(req,res)=>{
     res.sendFile(__dirname + "/views/index.html")
 
 })
 
 app.get("/about",(req, res)=>{
-    res.sendFile(__dirname + "/views/indexabout.html")
+    res.sendFile(__dirname + "/views/about.html")
 })
 
 app.get("/work",(req,res)=>{
-    res.sendFile(__dirname + "/views/indexworks.html")
+    res.sendFile(__dirname + "/views/work.html")
+})
+
+app.get("/gallery",(req, res) => {
+    res.sendFile(__dirname + "/views/gallery.html")
 })
 
 app.listen(port, () => {
